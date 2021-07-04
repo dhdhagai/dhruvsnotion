5e1f5ae91e53482517acf60846462e7009bf9b33
dhdhagai <84077442+dhdhagai@users.noreply.github.com>
2021-07-04 09:48:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const getVideos = require('./services/notion')
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 8000
 
 const app = express()
 
